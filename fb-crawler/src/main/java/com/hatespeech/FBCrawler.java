89d53d33f1248059fa3602185a5e7e02c7640945
Andreas Riegler <andreas.riegler@it-experts.at>
2017-05-04 21:48:17 +0200

    init

---
+++ fb-crawler/src/main/java/com/hatespeech/FBCrawler.java
@@ -0,0 +1,12 @@
+package com.hatespeech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FBCrawler {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FBCrawler.class, args);
+	}
+}
